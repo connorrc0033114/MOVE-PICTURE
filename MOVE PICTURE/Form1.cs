8f86b2fce8760b4c24219e2654228fcb08fcd389
Connor Harris <rc0033114@student.riversidecollege.ac.uk>
2018-11-20 10:39:37 +0000

    added move button

--- MOVE PICTURE/Form1.cs
+++ MOVE PICTURE/Form1.cs
@@ -16,5 +16,15 @@ namespace MOVE_PICTURE
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y);
+        }
     }
 }
